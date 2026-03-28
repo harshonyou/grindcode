cbf67918fb5099420f720f712daf31a9facb6701
Harsh Mohan <harshmohan813@gmail.com>
2026-03-28 01:46:38 +0000

    ~design_hashmap

--- Arrays & Hashing/easy/design_hashmap.py
+++ Arrays & Hashing/easy/design_hashmap.py
@@ -1,27 +1,51 @@
+class ListNode:
+    def __init__(self, key=0, val=0, next=None):
+        self.key = key
+        self.val = val
+        self.next = next
+
+
 class MyHashMap:
+    SIZE = 10000
 
     def __init__(self):
-        self.arr = []
+        self.data = [ListNode() for _ in range(self.SIZE)]
+
+    def offset(self, key: int) -> int:
+        return key % self.SIZE
 
     def put(self, key: int, value: int) -> None:
-        for idx in range(len(self.arr)):
-            k, v = self.arr[idx]
-            if k == key:
-                self.arr[idx] = (k, value)
-                break
-        else:
-            self.arr.append((key, value))
+        off = self.offset(key)
+
+        linkedList = self.data[off]
+        while linkedList.next:
+            if linkedList.next.key == key:
+                linkedList.next.val = value
+                return
+
+            linkedList = linkedList.next
+
+        linkedList.next = ListNode(key, value)
 
     def get(self, key: int) -> int:
-        for idx in range(len(self.arr)):
-            k, v = self.arr[idx]
-            if k == key:
-                return v
+        off = self.offset(key)
+
+        linkedList = self.data[off]
+        while linkedList.next:
+            if linkedList.next.key == key:
+                return linkedList.next.val
+
+            linkedList = linkedList.next
+
         return -1
 
     def remove(self, key: int) -> None:
-        for idx in range(len(self.arr)):
-            k, v = self.arr[idx]
-            if k == key:
-                self.arr[idx] = (k, -1)
+        off = self.offset(key)
+
+        linkedList = self.data[off]
+        while linkedList.next:
+            if linkedList.next.key == key:
+                linkedList.next = linkedList.next.next
                 return
+
+            linkedList = linkedList.next
