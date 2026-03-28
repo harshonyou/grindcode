2eba76410b7489063d257c347773fa9a4d444c88
Harsh Mohan <harshmohan813@gmail.com>
2026-03-28 01:06:28 +0000

    ~majority_element

--- Arrays & Hashing/easy/majority_element.py
+++ Arrays & Hashing/easy/majority_element.py
@@ -1,7 +1,21 @@
-from collections import Counter
-
-
 class Solution:
     def majorityElement(self, nums: list[int]) -> int:
-        c = Counter(nums)
-        return c.most_common(1)[0][0]
+        runningMajor = nums[0]
+        runningFreq = 1
+        ptr = 1
+
+        while ptr < len(nums):
+            num = nums[ptr]
+
+            if num == runningMajor:
+                runningFreq += 1
+            else:
+                runningFreq -= 1
+
+            if runningFreq <= 0:
+                runningMajor = num
+                runningFreq = 1
+
+            ptr += 1
+
+        return runningMajor
