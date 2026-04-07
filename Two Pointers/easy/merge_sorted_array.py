be3e858bc3cde4bf911a1ff607d89c47bc2f4f34
Harsh Mohan <harshmohan813@gmail.com>
2026-04-07 03:06:39 +0100

    ~merge_sorted_array

--- Two Pointers/easy/merge_sorted_array.py
+++ Two Pointers/easy/merge_sorted_array.py
@@ -1,21 +1,18 @@
 class Solution:
     def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
-        ptr1, ptr2 = 0, 0
-        nums = []
+        ptr1, ptr2 = m - 1, n - 1
+        ptr = m + n - 1
 
-        while ptr1 < m and ptr2 < n:
-            if nums1[ptr1] <= nums2[ptr2]:
-                nums.append(nums1[ptr1])
-                ptr1 += 1
+        while ptr1 >= 0 and ptr2 >= 0:
+            if nums1[ptr1] > nums2[ptr2]:
+                nums1[ptr] = nums1[ptr1]
+                ptr1 -= 1
             else:
-                nums.append(nums2[ptr2])
-                ptr2 += 1
+                nums1[ptr] = nums2[ptr2]
+                ptr2 -= 1
+            ptr -= 1
 
-        if ptr1 < m:
-            nums.extend(nums1[ptr1:])
-
-        if ptr2 < n:
-            nums.extend(nums2[ptr2:])
-
-        for idx in range(len(nums1)):
-            nums1[idx] = nums[idx]
+        while ptr2 >= 0:
+            nums1[ptr] = nums2[ptr2]
+            ptr2 -= 1
+            ptr -= 1
