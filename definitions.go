86a7b68cf5f1853223c9db6f422ee16a5f07e864
Harsh Mohan <harshmohan813@gmail.com>
2026-05-11 18:03:19 +0100

    add tree-node in difinitions

--- definitions.go
+++ definitions.go
@@ -1,6 +1,14 @@
 package main
 
+// ListNode is a node in a singly linked list.
 type ListNode struct {
 	Val  int
 	Next *ListNode
+}
+
+// TreeNode is a node in a binary tree.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
 }
