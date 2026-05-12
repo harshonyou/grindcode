b3e3a905791b5d1226ae0887be02355d7d268833
Harsh Mohan <harshmohan813@gmail.com>
2026-05-12 20:08:11 +0100

    sol same-tree

---
+++ 100.same-tree.go
@@ -0,0 +1,32 @@
+package main
+
+// @leet start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	// compare node, if either or nil, then false
+	// if value differ, then false
+	// if both nil, then true
+	// otherwise, compare children
+	if p == nil && q == nil {
+		return true
+	}
+
+	if p == nil || q == nil {
+		return false
+	}
+
+	if p.Val != q.Val {
+		return false
+	}
+
+	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
+
+// @leet end
