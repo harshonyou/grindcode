a0f760b961de3239bd4a2e82a313ad4e783ce7ce
Harsh Mohan <harshmohan813@gmail.com>
2026-05-13 19:14:42 +0100

    imp product-of-array-except-self

--- 238.product-of-array-except-self.go
+++ 238.product-of-array-except-self.go
@@ -6,42 +6,21 @@ func productExceptSelf(nums []int) []int {
 	// leftProduct, i -> nums0 * nums1 * ... * nums(i-1)
 	// rightProduct, i -> nums(i+1) * nums(i+2) * ... * nums(len-1)
 	// val we need -> leftProduct[i] * rightProduct[i]
-	idx := 1
 	n := len(nums)
+	res := make([]int, n)
 
-	leftProduct := make([]int, n)
-	rightProduct := make([]int, n)
-
-	leftProduct[0] = nums[0]
-	rightProduct[n-1] = nums[n-1]
-
-	for idx < n {
-		left := idx
-		right := n - idx - 1
-
-		leftProduct[left] = leftProduct[left-1] * nums[left]
-		rightProduct[right] = rightProduct[right+1] * nums[right]
-
-		idx++
+	res[0] = 1
+	for i := 1; i < n; i++ {
+		res[i] = res[i-1] * nums[i-1]
 	}
 
-	idx = 0
-	result := make([]int, n)
-	for idx < n {
-		left, right := 1, 1
-
-		if idx > 0 {
-			left = leftProduct[idx-1]
-		}
-		if idx < n-1 {
-			right = rightProduct[idx+1]
-		}
-
-		result[idx] = left * right
-		idx++
+	right := 1
+	for i := n - 1; i >= 0; i-- {
+		res[i] = res[i] * right
+		right *= nums[i]
 	}
 
-	return result
+	return res
 }
 
 // @leet end
